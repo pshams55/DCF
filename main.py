7096bffb6fd106769ef0f6fb9d07d536bfb7d575
pshams55 <53322721+pshams55@users.noreply.github.com>
2023-01-25 22:04:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+import cv2
+import random
+import numpy as np
+import torch
+import argparse
+from shutil import copyfile
+from src.config import Config
+from src.misf import DCF
+import torch.nn as nn
+
+
+def main(mode=None):
+
+
+    config = load_config(mode)
+
+
+    # cuda visble devices
+    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in config.GPU)
+
+
+    # init device
+    if torch.cuda.is_available():
+        config.DEVICE = torch.device("cuda")
+        torch.backends.cudnn.benchmark = True   # cudnn auto-tuner
+    else:
+        config.DEVICE = torch.device("cpu")
+
+
+
+    # set cv2 running threads to 1 (prevents deadlocks with pytorch dataloader)
+    cv2.setNumThreads(0)
+
+
+    # initialize random seed
+    torch.manual_seed(config.SEED)
+    torch.cuda.manual_seed_all(config.SEED)
+    np.random.seed(config.SEED)
+    random.seed(config.SEED)
+
+
+
+    # build the model and initialize
+    model = DCF(config)
+    model.load()
+
+    iteration = model.inpaint_model.iteration
+    if len(config.GPU) > 1:
+        print('GPU:{}'.format(config.GPU))
+        model.inpaint_model.generator = nn.DataParallel(model.inpaint_model.generator, config.GPU)
+        model.inpaint_model.discriminator = nn.DataParallel(model.inpaint_model.discriminator, config.GPU)
+
+    model.inpaint_model.iteration = iteration
+
+    # print(model.inpaint_model)
+
+
+
+    # model training
+    if config.MODE == 1:
+        # config.print()
+        print('\nstart training...\n')
+        model.train()
+
+    # model test
+    elif config.MODE == 2:
+        print('\nstart testing...\n')
+        model.test()
+
+
+def load_config(mode=None):
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '--checkpoints', type=str, default='./checkpoints', help='model checkpoints path (default: ./checkpoints)')
+
+    args = parser.parse_args()
+    config_path = os.path.join(args.path, 'config.yml')
+
+    if not os.path.exists(args.path):
+        os.makedirs(args.path)
+
+    config = Config(config_path)
+
+    # train mode
+    if mode == 1:
+        config.MODE = 1
+
+    # test mode
+    elif mode == 2:
+        config.MODE = 2
+
+
+    return config
+
+
+if __name__ == "__main__":
+    main()
