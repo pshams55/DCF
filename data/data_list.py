46b333ddc1004140e2a2f895801a6a7e033e03f5
pshams55 <53322721+pshams55@users.noreply.github.com>
2023-01-26 23:20:50 +0800

    Update data_list.py

--- data/data_list.py
+++ data/data_list.py
@@ -22,6 +22,3 @@ def scan(input_path, out_put):
 
     with open(out_put, 'w') as j:
         json.dump(result_list, j)
-
-scan('/Users/xiaoguangli/lxg/CV/publication/cvpr_2022/publication/code/misf/data/face', './face.txt')
-scan('/Users/xiaoguangli/lxg/CV/publication/cvpr_2022/publication/code/misf/data/mask', './mask.txt')
